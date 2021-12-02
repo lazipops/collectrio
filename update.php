9958f73b0f028b09aa40817d459d5670017cc270
lazipops <78057304+lazipops@users.noreply.github.com>
2021-12-02 00:13:43 -0500

    changes

--- update.php
+++ update.php
@@ -4,21 +4,7 @@ session_start();
 require('db.php');
 
 if (isset($_SESSION['username'])) { //check if user is logged in
-    if ($_POST['username'] != '' && $_POST['password'] != '') { //check if request fields are not empty
-        $username = mysqli_real_escape_string($con, $_POST['username']);
-        $password =  mysqli_real_escape_string($con, $_POST['password']);
-        $id = $_SESSION['id'];
-
-        $query = "UPDATE users SET username = '$username', passhash = '" . md5($password) . "' WHERE id = '$id'";
-        if ($con->query($query) === TRUE) {
-            $_SESSION['flash_message'] = 'Your account details have been changed';
-            $_SESSION['username'] = $_POST['username'];
-            header("Location: profile.php");
-        } else {
-            $_SESSION['flash_message'] = 'Error updating: ' . $con->error;
-            header("Location: profile.php");
-        }
-    } elseif ($_POST['username'] != '' && $_POST['password'] != '' && $_POST['email'] != '') {
+    if ($_POST['username'] != '' && $_POST['password'] != '' && $_POST['email'] != '') { //check if request fields are not empty
         $username = mysqli_real_escape_string($con, $_POST['username']);
         $password =  mysqli_real_escape_string($con, $_POST['password']);
         $email =  mysqli_real_escape_string($con, $_POST['email']);
@@ -33,7 +19,49 @@ if (isset($_SESSION['username'])) { //check if user is logged in
             $_SESSION['flash_message'] = 'Error updating: ' . $con->error;
             header("Location: profile.php");
         }
-    }elseif ($_POST['username'] != '') {
+    } elseif ($_POST['username'] != '' && $_POST['password'] != '') { //check if request fields are not empty
+        $username = mysqli_real_escape_string($con, $_POST['username']);
+        $password =  mysqli_real_escape_string($con, $_POST['password']);
+        $id = $_SESSION['id'];
+
+        $query = "UPDATE users SET username = '$username', passhash = '" . md5($password) . "' WHERE id = '$id'";
+        if ($con->query($query) === TRUE) {
+            $_SESSION['flash_message'] = 'Your account details have been changed';
+            $_SESSION['username'] = $_POST['username'];
+            header("Location: profile.php");
+        } else {
+            $_SESSION['flash_message'] = 'Error updating: ' . $con->error;
+            header("Location: profile.php");
+        }
+    } elseif ($_POST['username'] != '' && $_POST['email'] != '') {
+        $username = mysqli_real_escape_string($con, $_POST['username']);
+        $email =  mysqli_real_escape_string($con, $_POST['email']);
+        $id = $_SESSION['id'];
+
+        $query = "UPDATE users SET username = '$username', email = '$email' WHERE id = '$id'";
+        if ($con->query($query) === TRUE) {
+            $_SESSION['flash_message'] = 'Your account details have been changed';
+            $_SESSION['username'] = $_POST['username'];
+            header("Location: profile.php");
+        } else {
+            $_SESSION['flash_message'] = 'Error updating: ' . $con->error;
+            header("Location: profile.php");
+        }
+    } elseif ($_POST['password'] != '' && $_POST['email'] != '') {
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $email =  mysqli_real_escape_string($con, $_POST['email']);
+        $id = $_SESSION['id'];
+
+        $query = "UPDATE users SET email = '$email', passhash = '" . md5($password) . "' WHERE id = '$id'";
+        if ($con->query($query) === TRUE) {
+            $_SESSION['flash_message'] = 'Your account details have been changed';
+            $_SESSION['username'] = $_POST['username'];
+            header("Location: profile.php");
+        } else {
+            $_SESSION['flash_message'] = 'Error updating: ' . $con->error;
+            header("Location: profile.php");
+        }
+    } elseif ($_POST['username'] != '') {
         $username = mysqli_real_escape_string($con, $_POST['username']);
         $id = $_SESSION['id'];
 
