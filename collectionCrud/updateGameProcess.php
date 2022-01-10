cb1dc2effb3a9dcd252cd34ff18bbd5d72acbba1
lazipops <78057304+lazipops@users.noreply.github.com>
2022-01-10 17:17:34 -0500

    restricted game ratings from 0 to 100

--- collectionCrud/updateGameProcess.php
+++ collectionCrud/updateGameProcess.php
@@ -11,6 +11,12 @@ if (isset($_POST['changeRec'])) { //check for specific post request
     $platform = $_POST['platform'];
     $rating = $_POST['rating'];
 
+    if ($rating > 100) { //change rating to either 100 if it is over 100 or 0 if it is lower than 0
+        $rating = 100;
+    } else if ($rating < 0) {
+        $rating = 0;
+    }
+
     $result = mysqli_query($con, "UPDATE user" . $_SESSION['id'] . "_roms SET title='$title', genre='$genre', 
     releaseDate='$releaseDate', developer='$developer', platform='$platform', rating='$rating' WHERE id='$id'");
     if ($result) {
